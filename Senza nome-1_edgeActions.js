3dc19b00f1988d137c1ab55dc88ce36b0949abb9
steveacab <tony_not@hotmail.it>
2013-04-20 00:33:32 +0200

    nuovi file

---
+++ Senza nome-1_edgeActions.js
@@ -0,0 +1,34 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_plus}","click",function(sym,e){window.open("https://plus.google.com/105949072806429057852/posts","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_mail2}","click",function(sym,e){window.open("mailto:tony_not@hotmail.it","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_facebook}","click",function(sym,e){window.open("http://www.facebook.com/antonio.steveacab","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_twitter}","click",function(sym,e){window.open("https://twitter.com/steveacab","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"window","resize",function(sym,e){});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Secondo_1'
+(function(symbolName){})("Secondo_1");
+//Edge symbol end:'Secondo_1'
+
+//=========================================================
+
+//Edge symbol: 'Secondo_2'
+(function(symbolName){})("Secondo_2");
+//Edge symbol end:'Secondo_2'
+
+//=========================================================
+
+//Edge symbol: 'Secondo_3'
+(function(symbolName){})("Secondo_3");
+//Edge symbol end:'Secondo_3'
+})(jQuery,AdobeEdge,"EDGE-210057");
